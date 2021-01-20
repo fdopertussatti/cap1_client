4fa17dcc4b3e55fcdf5b543d76608b2f829b35a1
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-19 22:20:16 -0500

    Create Projetc

---
+++ backend/src/test/java/com/pertusoftware/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pertusoftware.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
