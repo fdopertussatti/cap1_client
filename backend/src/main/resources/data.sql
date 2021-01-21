ec54979a9ee3ad5d684cdddf2fe117020bf015dd
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-20 22:28:37 -0500

    Seeding Database

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,3 +4,7 @@ INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALU
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Pedro Lucas Biavatti', '00288209455', 100000, '1990-10-24T00:00:00.0Z'	,  1, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Carlos Bertoncelos', '23450009455', 67003.9, '1985-12-20T00:00:00.0Z'	,  2, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Francisco Cauda', '33288209455', 54600.0, '1980-07-10T00:00:00.0Z'	,  8, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Yury Alberto', '23523645267', 57650.0, '1989-09-1T00:00:00.0Z'	,  0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Felipe Martins', '19868820635', 60600.0, '1999-03-16T00:00:00.0Z'	,  1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Leonardo Couto', '00988209433', 90800.8, '1996-07-7T00:00:00.0Z'	,  0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES ('Marcos Lazarotto', '87658209462', 89600.0, '1984-4-4T00:00:00.0Z'	,  3, NOW());
