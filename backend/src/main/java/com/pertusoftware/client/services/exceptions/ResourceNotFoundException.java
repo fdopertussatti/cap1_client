0d5ef8e3b6e54b81f2af1e721e7564b6c1c2d9be
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-20 19:19:19 -0500

    Initial configurations and creat entity Client.

---
+++ backend/src/main/java/com/pertusoftware/client/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.pertusoftware.client.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
